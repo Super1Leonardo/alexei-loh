b083ef2fc3a7bff5c3856eac565a3d95a7b8a8f2
levabaluevvv <levabaluevvv@gmail.com>
2024-11-15 18:50:04 +0000

    first commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let title: string
+    export let theme: string
+    export let url: string
+</script>
+
+<a href={`/project`} class="space-y-3">
+    <img
+      alt={'project.title'}
+      style=''
+      src="{url}"
+      width={360}
+      height={240}
+      class=""
+    />
+    <div>
+      <div class=" font-sans">{title}</div>
+      <div class="text-neutral-500 text-sm">
+        {theme}
+      </div>
+    </div>
+  </a>
