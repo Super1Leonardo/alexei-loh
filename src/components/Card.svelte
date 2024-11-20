4f6b1a16b88eebb38876f86b28436bfc8fbce2c5
SupLeonardo Meow <levabaluevvv@gmail.com>
2024-11-20 13:32:15 +0000

    lev

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -4,16 +4,16 @@
     export let url: string
 </script>
 
-<a href={`/project`} class="space-y-3">
+<a href={`/project`} class=" border-gray-100 border-2 shadow-xl rounded-[36px] ">
     <img
       alt={'project.title'}
       style=''
       src="{url}"
       width={360}
       height={240}
-      class=""
+      class="rounded-[34px]  "
     />
-    <div>
+    <div class="px-6 pb-2 ">
       <div class=" font-sans">{title}</div>
       <div class="text-neutral-500 text-sm">
         {theme}
